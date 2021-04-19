6900d44f08bb2173b5d6ef335d0e202766b73dfe
Daniel <R3ddev1l81@gmail.com>
2021-04-19 18:46:06 -0400

    Added backend files

---
+++ backend/screenrecorder.py
@@ -0,0 +1,79 @@
+# To install
+# pip install pyautogui
+# pip install pytesseract
+# pip install NLTK
+# pip install opencv-python
+# Install Tesseract https://github.com/UB-Mannheim/tesseract/wiki
+# Set your OCR directory in line 31 (currently it is set up for my personal directory)
+
+
+# TODO 
+# Drop down box to get domain of presentation
+# Return definition based on domain, not fully implemented
+# Move code from while loop into a function call
+# Get user coordinates for screenshot
+# Get topic of presentation from slide 1
+# Return first def if specific definition isn't available
+# Return wiktionary link for the word in question, for frontend to use
+
+
+import pyautogui
+import cv2
+import numpy as np
+import time
+import pytesseract
+from PIL import Image
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from wiktionaryparser import WiktionaryParser
+
+#OCR Directory
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Nasty\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+sno = nltk.stem.SnowballStemmer('english')
+parser = WiktionaryParser()
+
+with open("wordList.txt") as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+lastText = ""
+slideNum = 0
+domain = "computing" # Have this domain be set by the user
+
+while True:
+    img = pyautogui.screenshot(region=(0,100,1280, 940)) # (left, top, width, height)
+
+    img.save("screenshot.png")
+    image = cv2.imread("screenshot.png")
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    im = Image.open("screenshot.png")
+    newText = pytesseract.image_to_string(im).strip().lower().split()
+
+    if (newText != lastText):
+        lastText = newText
+        print("Last deciphered text: ")
+        print(lastText)
+        slideNum = slideNum + 1
+        print("Slide Number: ")  
+        print(slideNum)
+        uncommonWordsOnSlide = [x for x in lastText if x not in content]
+        print("Uncommon words: ")
+        print(uncommonWordsOnSlide)
+        for i in uncommonWordsOnSlide:
+            specialDef = False # Set this to true if program finds a domain-specific definition
+            word = parser.fetch(i)
+            for k, v in word[0].items():
+                if (k == 'definitions'):
+                    listStuff = v[0].get('text')
+                    for i in listStuff:
+                        if domain == "computing" and "(computing)" in i: 
+                            print(i) # Prints the definition with (computing)
+                            specialDef = True
+                            break
+                    if specialDef == False: # If program does not find a definition specific to the domain
+                        print(listStuff) # This prints all definitions, can be changed to only first definition
+                    
+    time.sleep(2)
+
