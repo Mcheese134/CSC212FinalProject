955331448e46c0f2b383ba17a7c4eb5e2950cfb9
Felix <nurfajri@indoskill.com>
2021-04-25 18:21:21 -0400

    Commented the Code

--- Minerva/app.py
+++ Minerva/app.py
@@ -1,66 +1,60 @@
+#Retrieve dependencies
 from flask import Flask, render_template, request, jsonify;
 from flask_bootstrap import Bootstrap;
+
+
 import json
-
-
-
 import sys;
+
+#Add a system path to call backend functions
 sys.path.append("../backend")
 
-
+#Needs to be called after the system path - Python script located there
+from screenrecorder import screenRecord;
 from pynput.mouse import Button, Listener
 
-from screenrecorder import screenRecord;
 
+#Get X and Y Coordinates
+x_coords = []
+y_coords = []
 
-xs = []
-ys = []
+#Final list to append all (x,y) coordinates
+all_coords = []
 
-coord = []
-
+#Establish an event listener for clicks
 listener = 0
 
+#This function will activate listener for mouse clicks
 def activate():
   global listener
-  global coord
-  global xs
   listener = Listener(on_click=on_click)
   listener.start()
   listener.join()
 
 
 
-
-
-
-
-
+#This function will retrieve x and y coordinates up to two mouse clicks
 def on_click(x, y, button, pressed):
-    global xs
-    global ys
-    global coord
+    global x_coords
+    global y_coords
+    global all_coords
 
    
     if button == Button.left and pressed:
-        if len(xs) < 1 and len(ys) < 1:
-            xs.append(x)
-            ys.append(y)
-            print(xs, ys)
+        if len(x_coords) < 1 and len(y_coords) < 1:
+            x_coords.append(x)
+            y_coords.append(y)
         else:
-          xs.append(x)
-          ys.append(y)
-          coord.append(xs)
-          coord.append(ys)
+          x_coords.append(x)
+          y_coords.append(y)
+          coord.append(x_coords)
+          coord.append(y_coords)
           listener.stop()
           return False
 
             
   
-
-
-
 app = Flask(__name__)
-
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # No caching at all for API endpoints.
@@ -72,50 +66,42 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
+#Renders the landing page for the application
 @app.route("/")
 def index():
     return render_template('intro.html')
 
 
- #background process happening without any refreshing
+#background process happening without any refreshing
 @app.route('/background_process_test', methods=['GET', 'POST'])
 def background_process_test():
   global coord
   global xs
   global ys
 
+  #Reset coordinates each time a post request is made and retrieve new coords as a json string
   if request.method == 'POST':
-
- 
     coord = []
     xs = []
     ys = []
     activate()
-
-    print("coord: " + str(coord))
-
-
-
     return json.dumps(coord)
   
 
-
-
+#Renders the main applicaion page
 @app.route("/home")
 def intro():
  #Query String
   coord = request.cookies.get('coord')
 
-
-  #Backend Data
+  #Variable that holds all data on slide number, uncommon words found, and the matching description
   attributes = screenRecord(coord)
+
+  #Split into individual varibales
   slideNum = attributes[0]
   uncommonWords = attributes[1]
   desc = attributes[2]
 
-
-
-  print("UncommonWords: " + str(len(uncommonWords)))
   return render_template('index.html', slideNum = slideNum, uncommonWords = uncommonWords, desc = desc)  
 if __name__ == "__main__":
   app.run()
--- backend/screenrecorder.py
+++ backend/screenrecorder.py
@@ -9,132 +9,137 @@
 
 # TODO 
 # Drop down box to get domain of presentation
-# Get user coordinates for screenshot
 
 
+#Import all the dependencies
 import pyautogui
 import cv2
 import numpy as np
 import time
 import pytesseract
 import json
-from PIL import Image
 import nltk
-from nltk.stem.snowball import SnowballStemmer
-from wiktionaryparser import WiktionaryParser
 import subprocess
 
+from PIL import Image
+from nltk.stem.snowball import SnowballStemmer
+from wiktionaryparser import WiktionaryParser
 
 
 
 
+#This function will take the coordinates, screenshot, parse that screenshot for technical jargon, and output definitions/slideNum
 def screenRecord(screenCoord):
 
-    #OCR Directory
+    #OCR Directory - For Windows : Set your directory path
     #pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Nasty\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
+    #Can be used to get screen size 
     screen = pyautogui.size()
-    print("SCREEN: " + str(screen))
-
     
 
     #Data is split between in order: x coordinates array and y coordinate array. They are in order
     screenCoord =  json.loads(screenCoord)
 
+    #This will detect a retina display - Needs to be doubled because it has double pixels
     if subprocess.call("system_profiler SPDisplaysDataType | grep -i 'retina'", shell= True) == 0: 
             for i in range(len(screenCoord)):
                 for j in range(len(screenCoord[i])):
                    screenCoord[i][j] = screenCoord[i][j] * 2.0
 
-    print("I have coord: " + str(screenCoord))
+    #Split coordinates into these attributes
     top = screenCoord[1][0]
     bottom = screenCoord[1][1]
     left = screenCoord[0][0]
     right = screenCoord[0][1]
    
+   #Establish Width and Height
     width = abs(right - left)
     height = abs(top - bottom)
 
 
-    
-
-
+    #Initialize Wikitionary Parser
     sno = nltk.stem.SnowballStemmer('english')
     parser = WiktionaryParser()
 
     # Front-End Variable - This will get slideNum, definitions, and words
     frontend = []
 
+    #Retrieve all the common words
     with open("wordList.txt") as f:
         content = f.readlines()
     content = [x.strip() for x in content]
 
+    # Variable that will hold the last parsed text
     lastText = ""
+
+    #Current slide number when image changes
     slideNum = 0
-    domain = "computing" # Have this domain be set by the user
 
+    # Have this domain be set by the user   
+    domain = "computing" 
 
-
-    
-
+    #TODO: Make this automated for now. Need to refresh to update and only checks once
     count = 0
     while count != 1:
 
     
-    
+        #Take screenshot 
         img = pyautogui.screenshot(region=(left,top,width, height) )# (left, top, width, height)
         img.save("screenshot.png")
         img.save("static/screenshot.png")
 
-        # (left, top, width, height)
         
-        #This is for front-end
-
+        #Parse the screenshot that was established
         image = cv2.imread("screenshot.png")
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         threshold_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         im = Image.open("screenshot.png")
         newText = pytesseract.image_to_string(im).strip().lower().split()
 
+        #Only check for technical words if the text are different
         if (newText != lastText):
+
+            # New Text will be compared when new screenshot is made
             lastText = newText
-            #print("Last deciphered text: ")
-            #print(lastText)
+
+            #set the values for the data to be sent to frontend
             slideNum = slideNum + 1
-            #print("Slide Number: ")  
-           # print(slideNum)
             uncommonWordsOnSlide = [x for x in lastText if x not in content]
-            print("Uncommon words: ")
-            print(uncommonWordsOnSlide)
-
-            frontend.append(slideNum)
-
             desc = []
+
+            #This will filter out words that was not successfully parsed
             cannotParse = []
 
-
+            #Run through all known technical jargon
             for i in uncommonWordsOnSlide:
                 
                 specialDef = False # Set this to true if program finds a domain-specific definition
 
+                #Parse the word to see if it has any definitions
                 word = parser.fetch(i)
 
-
+                #Add to cannot parse if no data was collected
                 if(len(word) <= 0):
                     cannotParse.append(i)
                     continue
 
                 
+                #Loop through parsed data for definitions
                 for k, v in word[0].items():
+
+                    #Check if current item is definitions
                     if (k == 'definitions'):
 
+                        #If no definition found, then default to this
                         if(len(v) == 0):
-                            desc.append("Definition Not Found For: " + str(i))
+                            desc.append("Definition not found for: " + str(i))
                             break
-                        else:
-                            print("LENGTH OF V: " + str(len(v)))
 
+                        #Get the text for definitions
                         listStuff = v[0].get('text')
+
+                        #Run through all definitions and get special definitions based on selected domain
                         for i in listStuff:
                             if domain == "computing" and (("computing" in i) or ("computer science" in i)):
                                 print(i) # Prints the definition with (computing)
@@ -161,21 +166,22 @@ def screenRecord(screenCoord):
                                 specialDef = True
                                 desc.append(i)
                                 break
+                        #Display only the first defintion (index 0 is "not comparable" usually in this case)
                         if specialDef == False: # If program does not find a definition specific to the domain
                             print(listStuff) # This prints all definitions, can be changed to only first definition
                             desc.append(listStuff[1])
 
+            #Remove parsed words
             for dup in cannotParse:
                 uncommonWordsOnSlide.remove(dup)
-            print("UNCOMMON: " + str(uncommonWordsOnSlide))
-            frontend.append(uncommonWordsOnSlide)
 
+            #Append necessary data for frontend
+            frontend.append(slideNum)
+            frontend.append(uncommonWordsOnSlide)
             frontend.append(desc)
                         
         time.sleep(2)
         count+=1
-
-    print("I am done")
 
     return frontend
 
